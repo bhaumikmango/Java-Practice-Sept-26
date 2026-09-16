4aa3b5787774bc0395fed1a238f8341c13e97140
bhaumikmango <superbhaumik@gmail.com>
2026-09-16 14:14:51 +0530

    Arithmetic Operators

---
+++ Practice/b.java
@@ -0,0 +1,42 @@
+public class b {
+    public static void main(String [] args){
+        // Operators in Java
+         
+        // Arithmetic Operators
+        // int a = 5;
+        // int b = 10;
+        
+        // int c = a + b;
+        // int d = a - b;
+        // int e = a * b;
+        // int f = b / a;
+        // int g = b % a;
+
+        // System.out.println(a + "," + b + "," + c + "," + d + "," + e  + "," + f  + "," + g);
+
+        // int h = a + 2;
+        // h = h + 2;
+        // h += 2;
+        
+        // // Similarly other operators can be used in assignment
+        // System.out.println(h);
+
+        // int g = 3;
+        // g ++;
+        // System.out.println(g);
+
+        // Pre increment/decrement and post increment/decrement
+        int i = 7;
+        i ++; //Postfix
+        ++ i; //Prefix
+        System.out.println(i);
+        int j = ++i; // j = i + 1
+        System.out.println(j);
+        System.out.println(i);
+        int k = i++; // k = i; i = i + 1
+        System.out.println(k);
+        System.out.println(i);
+        
+
+    }
+}
