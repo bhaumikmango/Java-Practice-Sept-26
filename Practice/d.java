0fd00d610b3320bdc7755617f953f484a6b33854
bhaumikmango <superbhaumik@gmail.com>
2026-09-17 12:34:26 +0530

    Iterables (While, Do while, For, For each, Break, Continue, Nested Loops)

---
+++ Practice/d.java
@@ -0,0 +1,62 @@
+public class d {
+    public static void main(String[] args) {
+        // // Iterables 
+
+        // int i = 1;
+
+        // // While Loop
+        // while (i <= 10) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // // Do while loop (One execution without comparison)
+        // do {
+        //     System.out.println(i);
+        //     i++;
+        // } while (i <= 1);
+        
+        // // For loop
+        // for (i = 1; i <= 10; i++) {
+        //     System.out.println(i);
+        // }
+        
+        // // For Each Loop
+        // String[] fruits = {"Apple", "Banana", "Cherry"};
+
+        // for (String fruit : fruits) {
+        //     System.out.println(fruit);
+        // }
+
+        
+        // // Nested Loops
+        // for (int i = 1; i <= 5; i ++){
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("");
+        // }
+
+        // // Prime Number
+        // int p = 11;
+        // int i;
+        // for (i = 2; i < p; i ++){
+        //     if (p % i == 0){
+        //         System.out.println("Not a Prime Number");
+        //         break;
+        //     }
+        // }
+        // if (i == p) {
+        //     System.out.println("Prime Number");
+        // }
+
+        // // Skip even numbers
+        // for (int i = 0; i < 121; i++) {
+        //     if (i % 2 == 0) {
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        // }
+    
+    }
+}
