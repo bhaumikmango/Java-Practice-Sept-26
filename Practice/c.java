d2e6982d70c8de42669161799e32ebad45a7a756
bhaumikmango <superbhaumik@gmail.com>
2026-09-17 10:54:37 +0530

    Conditional Statements(if, else, nested if else, switch, nested switch)

---
+++ Practice/c.java
@@ -0,0 +1,44 @@
+public class c {
+    public static void main(String[] args) {
+        // // Conditional Statements
+        // boolean a = false;
+        // if (a){
+        //     System.out.println("true");
+        // }
+        // else{
+        //     System.out.println("false");
+        // }
+
+        // // Nested if
+        // int a = 11;
+        // if (a < 5){
+        //     System.out.println("Less than 5");
+        // }
+        // else if (a < 10){
+        //     System.out.println("Between 5 and 10");
+        // }
+        // else{
+        //     System.out.println("More than 10");
+        // }
+
+        // // Switch Statements (similar to nested else if but more optimized)
+        // // Jumps directly to variable's value execution block instead of one by one evaluation(like else if)
+        // switch (a) {
+        //     case 1:
+        //         System.out.println("A is 1");
+        //         break;
+        
+        //     case 2:
+        //         System.out.println("A is 2");
+        //         break;
+
+        //     default:
+        //         System.out.println("A is greater than 2");
+        //         break;
+        // }
+        // // Drawbacks - Can't operate with boolean evaluation
+        // // Uses only definite set, no ranges which can be used in else if
+        // // Can test equality but can't test inequality
+        // // Nested switch can be used as well for multiple variables
+    }
+}
