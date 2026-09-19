60f207255916232f131beb79c1df56a862ab3090
bhaumikmango <superbhaumik@gmail.com>
2026-09-19 18:28:02 +0530

    OOPs Basics

---
+++ Practice/h.java
@@ -0,0 +1,39 @@
+public class h {
+    public static void main(String[] args) {
+        // // OOPs in Java
+
+        // student s1 = new student();
+        // student s2 = new student();
+
+        // s1.age = 22;
+        // s1.name = "Bhaumik";
+        // s1.rollNumber = 15;
+        // s1.college = "IMSEC";
+
+        // s2.age = 22;
+        // s2.name = "Ashutosh";
+        // s2.rollNumber = 1;
+        // s2.college = "SISTEC";
+
+        // s1.markAttendance();
+        // s2.markAttendance();
+        
+        // s1.print();
+        // s2.print();
+    }
+}
+
+// class student{
+//     String name;
+//     int age;
+//     int rollNumber;
+//     String college;
+
+//     void markAttendance(){
+//         System.out.println("Attendance marked for " + name);
+//     }
+    
+//     void print() {
+//         System.out.println("Name " + name + "; Age " + age + "; Roll Number " + rollNumber + "; College Name " + college);
+//     }
+// }
